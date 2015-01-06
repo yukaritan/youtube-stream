6b18856fc6122f140ea03c37aa99e3fe2b2f68e9
yukaritan <yukaritan@users.noreply.github.com>
2015-01-06 17:55:37 +0100

    added wget as a fallback option

--- youtube-stream.sh
+++ youtube-stream.sh
@@ -7,3 +7,19 @@ filename=$(youtube-dl --get-filename "$1")
 echo "Saving to \"${filename}\""
 
 youtube-dl "$1" -o- | tee "${filename}" | mpv -
+
+
+# Catch all the exit codes to make sure everything went smoothly
+status=$(awk 'BEGIN {t=0; for (i in ARGV) t+=ARGV[i]; print t}' "${PIPESTATUS[@]}")
+
+if [ ${status} -ne "0" ]; then
+    echo "Something failed, falling back to wget"
+    
+    # Get the raw URL so we can continue fetching it with wget
+    url=$(youtube-dl --get-url "$1")
+    wget -c -O "${filename}" "${url}"
+
+else
+    echo "Everything went smoothly"
+    
+fi
